a78d3ca8216a065012c4071846ef59f09a17d639
Shawn Ren <shawnren527@gmail.com>
2019-04-05 03:44:42 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,2 +1,16 @@
 class Appointment
+  attr_reader :date, :doctor, :patient
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(date, doctor, patient)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
 end
