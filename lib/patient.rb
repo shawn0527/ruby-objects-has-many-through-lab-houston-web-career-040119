c75ca735c8c2e229a7f28ec20df1712140637be5
Shawn Ren <shawnren527@gmail.com>
2019-04-05 03:44:45 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,18 +1,5 @@
 class Patient
 
-  attr_reader :date, :doctor, :patient
-
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def initialize(date, doctor, patient)
-    @date = date
-    @doctor = doctor
-    @patient = patient
-    @@all << self
-  end
+  
 
 end
